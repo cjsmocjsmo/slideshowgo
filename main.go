7f481f7a0fb6ed59c3c3c41f15c7e3483fa3d3ce
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-07-31 02:42:12 -0700

    Refactor get_db_image to remove unused fields in SQL query

--- main.go
+++ main.go
@@ -62,9 +62,17 @@ func get_db_image(idx int) (ImageData, error) {
 	}
 	defer db.Close()
 
+	// var img ImageData
+	// query := "SELECT name, path, http, idx, orientation FROM images WHERE idx = ?"
+	// err = db.QueryRow(query, idx).Scan(&img.Name, &img.Path, &img.Http, &img.Idx, &img.Orientation)
+	// if err != nil {
+	// 	log.Printf("Error querying get_db_image: %v", err)
+	// 	return ImageData{}, err
+	// }
+	// return img, nil
 	var img ImageData
-	query := "SELECT name, path, http, idx, orientation FROM images WHERE idx = ?"
-	err = db.QueryRow(query, idx).Scan(&img.Name, &img.Path, &img.Http, &img.Idx, &img.Orientation)
+	query := "SELECT name, http, idx, orientation FROM images WHERE idx = ?"
+	err = db.QueryRow(query, idx).Scan(&img.Name, &img.Http, &img.Idx, &img.Orientation)
 	if err != nil {
 		log.Printf("Error querying get_db_image: %v", err)
 		return ImageData{}, err
